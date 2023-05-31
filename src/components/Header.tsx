22e74a3e7f13b63d091e9259cfe046e0e32ae2a7
Brandon Fernandez <fernandez.b@dhn.com.do>
2023-05-31 17:03:14 -0400

    button header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -46,6 +49,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         "
         >
           <button
+            onClick={() => router.back()}
             className="
                 rounded-full
                 bg-black
@@ -58,6 +62,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretLeft size={35} />
           </button>
           <button
+            onClick={() => router.forward()}
             className="
                 rounded-full
                 bg-black
@@ -69,6 +74,45 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           >
             <RxCaretRight size={35} />
           </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            className="
+            rounded-full
+            p-2
+            bg-white
+            flex
+            items-center
+            justify-center
+            hover:opacity-75
+            transition
+            "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+
+          <button
+            className="
+            rounded-full
+            p-2
+            bg-white
+            flex
+            items-center
+            justify-center
+            hover:opacity-75
+            transition
+            "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button />
+            </div>
+          </>
         </div>
       </div>
     </div>
